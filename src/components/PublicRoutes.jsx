9375caf89dda8171b73dc9ddc94aee57c2e4ea28
minhduc <minhductxqt@gmail.com>
2023-04-05 21:53:01 +0700

    fix bug auth route

--- src/components/PublicRoutes.jsx
+++ src/components/PublicRoutes.jsx
@@ -10,7 +10,7 @@ const PublicRoutes = () => {
   return (
     <>
       {auth ? (
-        <Navigate to={PATH.EXPLORE} state={{ from: location }} />
+        <Navigate to={PATH.HOME} state={{ from: location }} />
       ) : (
         <Outlet />
       )}
